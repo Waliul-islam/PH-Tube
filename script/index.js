fd3cadc3fabf584ff2f8ef4484b4f37c1b2ea86a
Waliul-islam <walirifat236@yahoo.com>
2025-03-13 02:30:55 +0600

    Video Details in Modal Added

--- script/index.js
+++ script/index.js
@@ -37,6 +37,35 @@ function loadCategoryVideos(id) {
         });
 }
 
+const loadVideoDetails =(videoId) => {
+    const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayVideoDetails(data.video));
+
+}
+
+const displayVideoDetails = (video) => {
+    document.getElementById("video_details").showModal();
+    const detailsContainer = document.getElementById("details-container");
+    detailsContainer.innerHTML = `
+    <div class="card bg-base-100 image-full w-96 shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title"> ${video.title} </h2>
+    <p> ${video.description} </p>
+    <div class="card-actions justify-end">
+    </div>
+  </div>
+</div>
+    `
+}
+
 function displayCategories(categories) {
   const categoryContainer = document.getElementById("category-container");
   for (let cat of categories) {
@@ -93,6 +122,7 @@ const displayVideos = (videos) => {
                 <p class="text-gray-400 text-base "> ${video.others.views} views </p>
               </div>
             </div>
+            <button onclick= loadVideoDetails('${video.video_id}') class="btn btn-block">Show Details</button>
           </div>
 
 
@@ -101,4 +131,4 @@ const displayVideos = (videos) => {
   });
 };
 
-    loadCategories();
+loadCategories();
